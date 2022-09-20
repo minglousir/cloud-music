37bb0038f4a63ea037eb906787e43da8b1f17aa5
minglousir <110614517+minglousir@users.noreply.github.com>
2022-09-20 14:43:25 +0800

    Update userInfo.js

--- static/userinfo/userInfo.js
+++ static/userinfo/userInfo.js
@@ -1,3 +1,9 @@
 export const userinfo = {
-
+"data": {
+    /*data请放在这*/
+  },
+  "cookies": {
+    "_ntes_nuid":"_ntes_nuid=8a1ca7dd86c3ec7169e37f49e8bd8374", "NMTID":"NMTID=00O0MYxEW-PcOirOkijtWp0zxaNC6EAAA", 
+    "MUSIC_U":"MUSIC_U=3558eabbc6d41eb745e101edc2f83e53a273faa5e443da7ad99367bcc0d51d201e8fa863b3c66636c159d9b6c03bd688d4dbf082a8813684"
+  }
 }
